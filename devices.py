64fd6df329329992359b9670c12511d22ea21c3e
Rodrigo H Padilla <Rodrigo.HernandezPadilla@charter.com>
2023-08-16 09:53:43 -0600

    V1.0

---
+++ devices.py
@@ -0,0 +1,21 @@
+user = "developer"
+password = "lastorangerestoreball8876"
+device_xe = {
+    "host": "sandbox-iosxe-recomm-1.cisco.com",
+    "username": user,
+    "password": password,
+    "device_type": "cisco_xe",
+}
+device_xr = {
+    "host": "sandbox-iosxr-1.cisco.com",
+    "username": "admin",
+    "password": "C1sco12345",
+    "device_type": "cisco_xr",
+}
+device_nx = {
+    "host": "sbx-nxos-mgmt.cisco.com",
+    "username": "admin",
+    "password": "Admin_1234!",
+    "device_type": "cisco_nxos",
+}
+device_list = [device_xe, device_xr, device_nx]
